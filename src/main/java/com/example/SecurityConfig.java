dbf6da68543200ce16f2fd40bcc1ac4f9302e3e1
superigera <super_rigera@icloud.com>
2022-11-18 06:40:03 +0900

    セキュリティ対応
    インメモリでの権限ページ切り分け
    各ページCSRF設定

---
+++ src/main/java/com/example/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationSuccessHandler AuthenticationSuccessHandler;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin@test.com").password(passwordEncoder().encode("123"))
+				.roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("user@test.com").password(passwordEncoder().encode("123")).roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginProcessingUrl("/login_page/input_login").loginPage("/login_page/input_login")
+				.usernameParameter("email").passwordParameter("password").successHandler(AuthenticationSuccessHandler);
+//		http.authorizeRequests().antMatchers("/register/**").authenticated();
+		// .hasRole("ADMIN").anyRequest().hasRole("USER");
+	}
+}
