36144ab5b7e71927d8973cf012c3c23afa8f0b2c
superigera <super_rigera@icloud.com>
2023-01-21 17:10:04 +0900

    管理者とユーザーのコントローラーを分割 #26

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,48 +1,13 @@
 package com.example.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.UserDetailsImpl;
-import com.example.application.UserApplicationService;
-import com.example.model.Member;
-import com.example.model.Menu;
-import com.example.model.Reserve;
-import com.example.service.MemberService;
-import com.example.service.MenuService;
-import com.example.service.ReserveService;
 
 @Controller
 @RequestMapping("/login_page")
 public class LoginController {
-
-	@Autowired
-	ReserveService reserveService;
-
-	@Autowired
-	MenuService menuService;
-
-	@Autowired
-	private UserApplicationService userApplicationService;
-
-	@Autowired
-	MemberService memberService;
-
-	@Autowired
-	private ModelMapper modelMapper;
 
 	// ログイン画面
 	@GetMapping("/input_login")
@@ -53,130 +18,6 @@ public class LoginController {
 	@PostMapping("/input_login")
 	public String postLogin() {
 		return "login/login";
-	}
-
-	// マイページ画面
-	@GetMapping("/mypage")
-	public String mypage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		// 日付取得
-		Date d = new Date();
-		SimpleDateFormat d1 = new SimpleDateFormat("yyyyMMdd");
-		String today = d1.format(d);
-
-		List<Reserve> ReserveList = reserveService.findReserve(userDetails.getMember_id(), today);
-		List<Menu> menus = new ArrayList<>();
-		for (Reserve reserve : ReserveList) {
-			menus = menuService.SearchMenu(reserve.getMenu_id());
-			for (Menu menu : menus) {
-				String name = menu.getMenu_name();
-				Integer times = menu.getMenu_times();
-				reserve.setMenu_name(name);
-				reserve.setMenu_times(times);
-			}
-		}
-		model.addAttribute("ReserveList", ReserveList);
-
-		List<Reserve> pastReserveList = reserveService.findPastReserve(userDetails.getMember_id(), today);
-		model.addAttribute("pastReserveList", pastReserveList);
-
-		return "login/mypage";
-	}
-
-	// マイページ画面キャンセル
-	@PostMapping("/mypage_cancel")
-	public String mypage_cancel(Model model, @RequestParam("cancel_id") String cancel_id) {
-
-		// 予約キャンセル
-		reserveService.cancelReserve(cancel_id);
-		return "redirect:/login_page/mypage";
-	}
-
-	// 個人情報更新画面
-	@GetMapping("/update_info")
-	public String update_info(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		Member member_info_list = memberService.findMember(userDetails.getMember_id());
-		model.addAttribute("member_info_list", member_info_list);
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
-		model.addAttribute("genderMap", genderMap);
-		return "login/update_info";
-	}
-
-	// DB更新
-	@PostMapping("/update_info")
-	public String update(Model model, Member member_list_info, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-		Member member = modelMapper.map(member_list_info, Member.class);
-		memberService.updateMember(member);
-		return "login/mypage";
-	}
-
-	// アドミン画面
-	@GetMapping("/admin")
-	public String admin(Model model) {
-		// 日付取得
-		Date d = new Date();
-		SimpleDateFormat d1 = new SimpleDateFormat("yyyyMMdd");
-		SimpleDateFormat d2 = new SimpleDateFormat("Hmm");
-		String today = d1.format(d);
-		String time = d2.format(d);
-
-		// 本日予約全件取得
-		List<Reserve> ReserveList = reserveService.findTodayReserve(today);
-		model.addAttribute("ReserveList", ReserveList);
-		model.addAttribute("time", time);
-		System.out.println(time);
-		System.out.println(ReserveList);
-
-		return "login/admin";
-	}
-
-	// アドミン画面検索
-	@PostMapping("/admin_search")
-	public String admin_sarch(Model model, @RequestParam("search_day") String search_day,
-			@RequestParam("search_name") String search_name) {
-		search_day = search_day.replace("-", "");
-
-		// 日付取得
-		Date d = new Date();
-		SimpleDateFormat d2 = new SimpleDateFormat("Hmm");
-		String time = d2.format(d);
-		model.addAttribute("time", time);
-
-		// 検索予約取得
-		List<Reserve> ReserveList = reserveService.findSearchReserve(search_day, search_name);
-		model.addAttribute("ReserveList", ReserveList);
-
-		return "login/admin";
-	}
-
-	// アドミン画面キャンセル
-	@PostMapping("/admin_cancel")
-	public String admin_cancel(Model model, @RequestParam("cancel_id") String cancel_id) {
-
-		// 予約キャンセル
-		reserveService.cancelReserve(cancel_id);
-		return "redirect:/login_page/admin";
-
-	}
-
-	// 予約登録画面
-	@GetMapping("/register")
-	public String register(Model model, Reserve reserve) {
-		// メニュー一覧取得
-		List<Menu> menuList = menuService.getMenus();
-		model.addAttribute("menuList", menuList);
-
-		return "login/reserve_register";
-	}
-
-	// DB登録
-	@PostMapping("/reserve_register")
-	public String reserve_register(Model model, Reserve reserve, @RequestParam("menu_id") Integer menu_id,
-			@RequestParam("reserve_date") String reserve_date, @RequestParam("time") String time) {
-		reserve_date = reserve_date.replace("-", "");
-		reserveService.reservations(menu_id, reserve.getNon_member_name(), null, reserve.getNon_member_phone_number(),
-				reserve_date, time);
-
-		return "redirect:/login_page/admin";
 	}
 
 }
